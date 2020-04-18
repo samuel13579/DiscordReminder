c6d1fd71e0e4d3f6231cc653f6c9ca2a25c1701d
Kitkat <samuel32259@gmail.com>
2020-04-18 14:45:48 -0400

    Got some basic commands set up, will need to changeto a command handler

--- server.js
+++ server.js
@@ -8,14 +8,44 @@ const config = {
   prefix: process.env.PREFIX
 };
 
+const prefix = config.prefix;
+
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+
+  if(msg.author.bot) return;
+  if(msg.content.indexOf(prefix) !== 0) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLocaleLowerCase();
+
+  switch (command) {
+    case 'ping': {
+      msg.reply('Pong!');
+      break;
+    }
+
+    case 'myname': {
+      const name = msg.member.displayName;
+      msg.delete();
+      msg.channel.send(`Your name is ${name}`);
+      break;
+    }
+
+    case 'say': {
+      const response = args.join(` `);
+      msg.channel.send(response);
+      break;
+    }
+
+    default:
+    msg.channel.send('This command is unknown');
+    break;
   }
+
 });
 
 bot.login(config.token);
